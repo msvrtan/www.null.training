69d235bf1c6f125e63b55da57f39e019e7720af8
Miro Svrtan <miro@mirosvrtan.me>
2017-12-17 22:40:31 +0100

    Task #53: Default config for javiereguiluz/easyadmin-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     FOS\UserBundle\FOSUserBundle::class => ['all' => true],
+    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
 ];
