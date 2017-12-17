b13dded7654edfd2c785a39551fdab66b099472f
Miro Svrtan <miro@mirosvrtan.me>
2017-12-17 16:40:50 +0100

    Task #40: Move homepage to /%locale%/ and redirect based on prefered language from "/"

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -6,15 +6,38 @@ namespace App\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Router;
 
 class HomepageController
 {
+    /** @var Router */
+    private $router;
+
+    public function __construct(Router $router)
+    {
+        $this->router = $router;
+    }
+
     /**
      * @Route("/")
+     */
+    public function indexAction(Request $request)
+    {
+        $preferredLanguage = $request->getPreferredLanguage(['en', 'hr']);
+
+        $url = $this->router->generate('homepage', ['_locale' => $preferredLanguage]);
+
+        return new RedirectResponse($url, 302);
+    }
+
+    /**
+     * @Route("/{_locale}/",name="homepage",requirements={"_locale":"en|hr"})
      * @Template()
      */
-    public function indexAction()
+    public function homepageAction()
     {
         return [];
     }
