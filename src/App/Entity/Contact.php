947aa67c633f6523e0eb7602ea884ea47038581d
Miro Svrtan <miro@mirosvrtan.me>
2018-01-13 11:06:25 +0100

    Feat #79: Add Contact storage
    For users to be able to contact us
    We need to store those contact inquiries

---
+++ src/App/Entity/Contact.php
@@ -0,0 +1,98 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use DateTime;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @see ContactFixtures
+ *
+ * @ORM\Entity(repositoryClass="App\Repository\ContactRepository")
+ */
+class Contact
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     *
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string",length=200)
+     *
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string",length=200)
+     *
+     * @var string
+     */
+    private $emailAddress;
+
+    /**
+     * @ORM\Column(type="text")
+     *
+     * @var string
+     */
+    private $body;
+
+    /**
+     * @ORM\Column(type="datetime")
+     *
+     * @var DateTime
+     */
+    private $createdAt;
+
+    public function __construct()
+    {
+        $this->createdAt = new DateTime();
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name)
+    {
+        $this->name = $name;
+    }
+
+    public function getEmailAddress(): string
+    {
+        return $this->emailAddress;
+    }
+
+    public function setEmailAddress(string $emailAddress)
+    {
+        $this->emailAddress = $emailAddress;
+    }
+
+    public function getBody(): string
+    {
+        return $this->body;
+    }
+
+    public function setBody(string $body)
+    {
+        $this->body = $body;
+    }
+
+    public function getCreatedAt(): DateTime
+    {
+        return $this->createdAt;
+    }
+}
